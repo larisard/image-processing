0ffc1a509a44c41f7c5209b07138afd0d16808fa
Larissa Sardinha <larissaribeirosardinha@gmail.com>
2025-03-28 14:49:25 -0300

    Atualizando a exibição da imagem

--- index.py
+++ index.py
@@ -1,15 +1,21 @@
 import cv2 
 import numpy as np
+import matplotlib.pyplot as plt
 
 img = cv2.imread('livraria.jpg', 1)
 imgCinza = cv2.imread('livraria.jpg', 0)
 
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-cv2.imshow("Minha Imagem", img)
 
-cv2.imshow("Imagem cinza", imgCinza)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+plt.figure(figsize=(10, 5))
+ 
+plt.subplot(121),plt.imshow(img_rgb)
+plt.title('Imagem inicial'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(imgCinza, cmap = 'gray')
+plt.title('Imagem tom cinza'), plt.xticks([]), plt.yticks([])
+plt.show()
+
 
 
 
